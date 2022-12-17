abdfb5eb360b6f9c53d9aad4c8328b9bfd0b660b
KakaniEmpires10 <muhammadalimkakani@gmail.com>
2022-12-17 16:49:46 +0700

    part 2

--- src/component/app.jsx
+++ src/component/app.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Header from './header';
 import Footer from './footer';
-import Note from './note'
+import Note from './note';
+import notes from "../note";
 
 function App() {
     return <div>
         <Header />
+        {notes.map(noteList => <Note 
+            key = {noteList.id}
+            title = {noteList.title}
+            content = {noteList.content}
+            />
+        )}
         <Footer />
-        <Note />
     </div>
 }
 
