f4067f9685c5dde254b01d393cfdc3524933bbe4
KakaniEmpires10 <muhammadalimkakani@gmail.com>
2022-12-17 16:17:53 +0700

    part 1

---
+++ src/component/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+    return <header>
+        <h1>Keeper App 🤳</h1>
+    </header>
+}
+
+export default Header;
---
+++ src/component/note.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Note() {
+    return <div className="note">
+        <h1>My Note</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, earum.</p>
+    </div>
+}
+
+export default Note;
